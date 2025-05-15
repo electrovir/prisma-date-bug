1322538b851a83075685d7df412846a5d9f79be8
electrovir <electrovir@users.noreply.github.com>
2025-05-15 12:40:30 +0000

    dates and Decimals are fixed in now in prisma 6.8

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import {PrismaBetterSQLite3} from '@prisma/adapter-better-sqlite3';
 import {PrismaClient} from './generated/index';
 import {databasePath, readUserDirectly} from './sqlite';
+import {Decimal} from '@prisma/client/runtime/client';
 
 const createdUser = await new PrismaClient({
         adapter: new PrismaBetterSQLite3({
             url: databasePath
         })
-    }).user.create({data: {}})
+    }).user.create({data: {
+        value: 42
+    }})
     
 console.log('from PrismaClient:');
 console.log(createdUser);
@@ -15,4 +18,6 @@ console.log(readUserDirectly());
 
 if (!(createdUser.createdAt instanceof Date) && !Reflect.ownKeys(createdUser.createdAt).length) {
     throw new Error('createdAt is empty');
+} else if (!(createdUser.value instanceof Decimal)) {
+    throw new Error('Decimal still a number')
 }
